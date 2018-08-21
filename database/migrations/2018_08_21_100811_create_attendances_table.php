1f67a31a3414b7ea0472b6b356face15a29f0aa0
chocochino <chocolate.coffee@outlook.com>
2018-08-21 10:19:08 +0000

    Create Attendance model

---
+++ database/migrations/2018_08_21_100811_create_attendances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('employee_id');
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->date('date');
+            $table->time('time');
+            $table->enum('status', ['time in', 'time out', 'sick leave', 'paid leave', 'unpaid leave', 'business trip']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
